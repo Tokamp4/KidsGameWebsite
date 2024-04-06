db595c539c59f1ba261d4dba5d4bb4eb673846aa
Thiago Padilha <128748914+tpneto@users.noreply.github.com>
2024-04-05 22:55:06 -0400

    add css

--- public/form/Last_Page.php
+++ public/form/Last_Page.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Congratulations!</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+    <link rel="stylesheet" href="../assets/css/main.css">
+    <link rel="stylesheet" href="../assets/css/index.css">
     <style>
         body {
             font-family: Arial, sans-serif;
