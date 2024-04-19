393d3ef238c49c79d8e1dd8e7b73a06cf3e377b9
Eduardo Tokarski <eduardo.tokarski.almeida@gmail.com>
2024-04-18 21:19:45 -0400

    ajax updates

--- public/form/signup-form.php
+++ public/form/signup-form.php
@@ -9,7 +9,7 @@ include('../template/footer.php')
 ?>
 <!-- Your HTML form code goes here -->
 
-?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -18,6 +18,11 @@ include('../template/footer.php')
     <link href="https://fonts.googleapis.com/css?family=Ubuntu:400,700" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="../assets/css/main.css">
+    <script type = "text/javascript" src = "/WebServerProject_Winter2024/public/assets/js/signup-onkeyup/fname-ajax.js"></script>
+    <script type = "text/javascript" src = "/WebServerProject_Winter2024/public/assets/js/signup-onkeyup/lname-ajax.js"></script>
+    <script type = "text/javascript" src = "/WebServerProject_Winter2024/public/assets/js/signup-onkeyup/uname-ajax.js"></script>
+    <script type = "text/javascript" src = "/WebServerProject_Winter2024/public/assets/js/signup-onkeyup/pcode1-ajax.js"></script>
+    <script type = "text/javascript" src = "/WebServerProject_Winter2024/public/assets/js/signup-onkeyup/pcode2-ajax.js"></script>
 </head>
 <body>
     <div class="menu-btn">
@@ -46,23 +51,33 @@ include('../template/footer.php')
 
                         <div class="form-group">
                             <label for="firstName">First Name</label>
-                            <input type="text" class="form-control" id="firstName" placeholder="First Name" name="firstName">
+                            <input type="text" class="form-control" id="firstName" placeholder="First Name" name="firstName"
+                            required onkeyup="validateFirstName()">
+                            <p><span class="" id=fnameHint></span></p>
                         </div>
                         <div class="form-group">
                             <label for="lastName">Last Name</label>
-                            <input type="text" class="form-control" id="lastName" placeholder="Last Name" name="lastName">
+                            <input type="text" class="form-control" id="lastName" placeholder="Last Name" name="lastName"
+                            required onkeyup="validateLastName()">
+                            <p><span id="lnameHint"></span></p>
                         </div>
                         <div class="form-group">
                             <label for="username">Username</label>
-                            <input type="text" class="form-control" id="username" placeholder="Username" name="username">
+                            <input type="text" class="form-control" id="username" placeholder="Username" name="username"
+                            required onkeyup="validateUsername()">
+                            <p><span id=usernameHint></span></p>
                         </div>
                         <div class="form-group">
                             <label for="password">Password</label>
-                            <input type="password" class="form-control" id="password" placeholder="Password" name="password">
+                            <input type="password" class="form-control" id="password" placeholder="Password" name="password"
+                            required onkeyup="validatePcode1()">
+                            <p><span id="pcode1Hint"></span></p>
                         </div>
                         <div class="form-group">
                             <label for="confirmPassword">Confirm Password</label>
-                            <input type="password" class="form-control" id="confirmPassword" placeholder="Confirm Password" name="confirmPassword">
+                            <input type="password" class="form-control" id="confirmPassword" placeholder="Confirm Password" name="confirmPassword"
+                            required onkeyup="validatePcode2()">
+                            <p><span id="pcode2Hint"></span></p>
                         </div>
                         <button type="submit" class="btn btn-default" name="register">Register</button>
                     </form>
